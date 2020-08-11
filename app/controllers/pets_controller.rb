6277fb3c5ae78a9d89f8bcd856edc54b3263e927
Hugo Daniel <hugodaniel.icam@gmail.com>
2020-08-11 18:07:47 +0200

    Create action for pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,4 +8,26 @@ class PetsController < ApplicationController
     # puts params => {"controller"=>"pets", "action"=>"show", "id"=>"1"}
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    # 1. Créer un Pet avec les params autorisés
+    @pet = Pet.new(pet_params)
+    # 2. Le sauvegarder
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+    # 3. Rediriger vers la page de détails du nouveau pet
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :address, :found_date)
+  end
 end
