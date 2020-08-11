a78e601ad54dbfaaf6fd095f6d580dd7f119b40f
Hugo Daniel <hugodaniel.icam@gmail.com>
2020-08-11 17:33:01 +0200

    Seeds for pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+Pet.create!(name: "Snoop", species: "dog", address: "Compton", age: 50, found_date: Date.today - 1.day)
+Pet.create!(name: "Catwoman", species: "cat", address: "Gotham", age: 25, found_date: Date.today - 2.day)
+Pet.create!(name: "Roger", species: "rabbit", address: "La Garenne", age: 3, found_date: Date.today - 1.week)
